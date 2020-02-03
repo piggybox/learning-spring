fdf67191ffa82d3a63b04c3e18df7532b7bd8e3e
Sky Yin <sky.yin@gmail.com>
2020-02-03 03:29:14 -0800

    Chapter 10, database

--- src/test/java/com/feelingSpring/FeelingApplicationTests.java
+++ src/test/java/com/feelingSpring/FeelingApplicationTests.java
@@ -1,13 +1,23 @@
 package com.feelingSpring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootTest
 class FeelingSpringApplicationTests {
 
+	// 注入数据源对象
+	@Autowired
+	private DataSource defaultDataSource;
+
 	@Test
-	void contextLoads() {
+	public void datasourceTest() throws SQLException {
+		System.out.println("默认数据源为：" + defaultDataSource.getClass());
 	}
 
 }
