058fbb4cf8ea2fe4e39f6f26138bb2e49c5097cd
Sky Yin <sky.yin@gmail.com>
2020-02-03 04:16:31 -0800

    Chapter 11: mybatis is still verbose

---
+++ src/main/java/com/feelingSpring/dao/UserDao.java
@@ -0,0 +1,39 @@
+package com.feelingSpring.dao;
+
+import com.feelingSpring.entity.*;
+
+/**
+ * @author 13 MyBatis 测试
+ */
+public interface UserDao {
+    /**
+     * 返回数据列表
+     *
+     * @return
+     */
+    List<User> findAllUsers();
+
+    /**
+     * 添加
+     *
+     * @param User
+     * @return
+     */
+    int insertUser(User User);
+
+    /**
+     * 修改
+     *
+     * @param User
+     * @return
+     */
+    int updUser(User User);
+
+    /**
+     * 删除
+     *
+     * @param id
+     * @return
+     */
+    int delUser(Integer id);
+}
