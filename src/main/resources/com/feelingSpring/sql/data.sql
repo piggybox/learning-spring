38c5e252b62e09885edb9dd798c21ae005dbc9af
Sky Yin <sky.yin@gmail.com>
2020-10-03 03:26:34 -0700

    fix: bug in query on repeated test

--- src/main/resources/com/feelingSpring/sql/data.sql
+++ src/main/resources/com/feelingSpring/sql/data.sql
@@ -1,9 +1,7 @@
-INSERT INTO
-    `test`.`ingredient` (`id`, `name`, `type`)
+REPLACE INTO `test`.`ingredient` (`id`, `name`, `type`)
 VALUES
     (1, 'test1', 'test1');
 
-INSERT INTO
-    `test`.`ingredient` (`id`, `name`, `type`)
+REPLACE INTO `test`.`ingredient` (`id`, `name`, `type`)
 VALUES
     (2, 'test2', 'test2');
