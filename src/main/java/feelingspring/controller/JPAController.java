03c83182b27b46b3401d979a556ca86a252d1171
Sky Yin <sky.yin@gmail.com>
2020-10-03 17:34:55 -0700

    restful twink

--- src/main/java/feelingspring/controller/JPAController.java
+++ src/main/java/feelingspring/controller/JPAController.java
@@ -2,21 +2,28 @@ package feelingspring.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import feelingspring.entity.Ingredient;
 import feelingspring.repository.IngredientRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 @RestController
+@RequestMapping(path = "/", produces = "application/json")
+@CrossOrigin(origins = "*")
 public class JPAController {
 
     @Autowired
     private IngredientRepository inRepo;
 
-    @GetMapping("/queryAll")
-    public List<Ingredient> queryAll() {
-        return inRepo.findAllByNativeQuery();
+    @GetMapping("/query/{id}")
+    public ResponseEntity<List<Ingredient>> query(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(inRepo.findAllByNativeQuery(), HttpStatus.OK);
     }
 }
--- src/main/java/feelingspring/repository/IngredientRepository.java
+++ src/main/java/feelingspring/repository/IngredientRepository.java
@@ -2,11 +2,11 @@ package feelingspring.repository;
 
 import java.util.List;
 
-import feelingspring.entity.Ingredient;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.Nullable;
+
+import feelingspring.entity.Ingredient;
 
 public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
     @Nullable
